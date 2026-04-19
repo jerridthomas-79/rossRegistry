52d0baf54388af04c51719226697e588ce896612
jerridthomas-79 <jerrid.thomas@gmail.com>
2026-04-19 12:04:55 -0500

    Change Supabase_ANON_KEY in config.js
    Updated the Supabase anonymous key in the configuration.

--- config.js
+++ config.js
@@ -3,6 +3,6 @@ window.APP_CONFIG = { SUPABASE_URL: "", SUPABASE_ANON_KEY: "", DEFAULT_PASSCODE:
 // config.js
 window.APP_CONFIG = {
   SUPABASE_URL: "https://avieoongmwrdmurpdart.supabase.co",
-  SUPABASE_ANON_KEY: "YOUR_ANON_KEY",
+  SUPABASE_ANON_KEY: "sb_publishable_kKn_J3OksjHe8boKNRiwrw_mKU7bSv0",
   DEFAULT_PASSCODE: "1234"
 };
