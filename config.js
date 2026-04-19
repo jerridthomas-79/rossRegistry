f93dd743065603feed6102b5cfed1a786e012abb
jerridthomas-79 <jerrid.thomas@gmail.com>
2026-04-19 12:08:16 -0500

    Clean up Supabase config

--- config.js
+++ config.js
@@ -1,6 +1,3 @@
-window.APP_CONFIG = { SUPABASE_URL: "", SUPABASE_ANON_KEY: "", DEFAULT_PASSCODE: "1234" };
-
-// config.js
 window.APP_CONFIG = {
   SUPABASE_URL: "https://avieoongmwrdmurpdart.supabase.co",
   SUPABASE_ANON_KEY: "sb_publishable_kKn_J3OksjHe8boKNRiwrw_mKU7bSv0",
